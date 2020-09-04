bfb616afb6db1d41176703dd96ffa831461804c4
Masahiro Honda <honda@mechatrax.com>
2020-09-04 15:10:24 +0900

    Update vars

--- goxc.go
+++ goxc.go
@@ -48,9 +48,9 @@ var (
 	// e.g. go build -ldflags "-X main.VERSION 0.1.2-abcd" goxc.go
 	// thanks to minux for this advice
 	// So, goxc does this automatically during 'go build'
-	VERSION     = "0.17.1"
+	VERSION     = "0.18.1"
 	BUILD_DATE  = ""
-	SOURCE_DATE = "2015-06-21T21:42:03+12:00"
+	SOURCE_DATE = "2020-09-04T14:56:29+09:00"
 	// settings for this invocation of goxc
 	settings             config.Settings
 	fBuildSettings       config.BuildSettings
